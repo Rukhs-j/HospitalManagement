7a2b670efc1ded6b1f923c33db9f1e080b1c0144
Jamadar Rukhsar <68462284+Rukhs-j@users.noreply.github.com>
2020-08-09 13:26:23 +0530

    Add files via upload

---
+++ db_hospital.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: Aug 09, 2020 at 09:54 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_tbl`
+--
+-- Error reading structure for table db_hospital.doctor_tbl: #1932 - Table 'db_hospital.doctor_tbl' doesn't exist in engine
+-- Error reading data for table db_hospital.doctor_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_hospital`.`doctor_tbl`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facilities`
+--
+-- Error reading structure for table db_hospital.facilities: #1932 - Table 'db_hospital.facilities' doesn't exist in engine
+-- Error reading data for table db_hospital.facilities: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_hospital`.`facilities`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab_tbl`
+--
+-- Error reading structure for table db_hospital.lab_tbl: #1932 - Table 'db_hospital.lab_tbl' doesn't exist in engine
+-- Error reading data for table db_hospital.lab_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_hospital`.`lab_tbl`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicine_tbl`
+--
+-- Error reading structure for table db_hospital.medicine_tbl: #1932 - Table 'db_hospital.medicine_tbl' doesn't exist in engine
+-- Error reading data for table db_hospital.medicine_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_hospital`.`medicine_tbl`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients_tbl`
+--
+-- Error reading structure for table db_hospital.patients_tbl: #1932 - Table 'db_hospital.patients_tbl' doesn't exist in engine
+-- Error reading data for table db_hospital.patients_tbl: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_hospital`.`patients_tbl`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
